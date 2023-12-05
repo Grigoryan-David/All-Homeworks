f55fa7c3cdd04e9f5ce39fe56b9e581d84fa4169
Grigoryan-David <103514386+Grigoryan-David@users.noreply.github.com>
2023-12-05 13:13:39 +0400

    Add files via upload

---
+++ Boyer-Moore.py
@@ -0,0 +1,30 @@
+text = "a pattern matching aimithm"
+pattern =               "mithm"
+
+def BoyerMoore(text, pattern):
+    m = len(pattern)
+    n = len(text)
+    skip = {}
+    for i in range(m - 1):
+        skip[pattern[i]] = m - i - 1
+    if pattern[-1] in pattern[:-1]:
+      skip.update([("*", m)])
+    else:
+      skip.update([(pattern[-1], m), ("*", m)])
+    i = 0
+    while i <= n - m:
+      j = m - 1
+      while j >= 0:
+        print(text[i+j], pattern[j])
+        if text[i + j] not in skip.keys():
+          i += skip["*"] - (m-1-j)
+          break
+        elif text[i + j] != pattern[j]:
+          i += skip[text[i + j]]
+          break
+        j -= 1
+      if j == -1:
+        return i
+    return -1
+
+print(BoyerMoore(text, pattern))
